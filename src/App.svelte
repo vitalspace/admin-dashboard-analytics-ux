ee51d0176bb06d6ee55e4cd54edf57a89710c7c8
vitalspace <myglobalsounds@gmail.com>
2023-07-15 01:06:59 -0600

    updated

--- src/App.svelte
+++ src/App.svelte
@@ -20,27 +20,24 @@
 
   <div class="absolute inset-0 ml-72 border-2 border-red-500">
     <div class="flex fixed inset-0 ml-72">
-      <div class="flex w-1/2 flex-wrap items-center border-2 px-8 py-8">
+      <div class="flex w-1/2 flex-wrap items-center space-y-4 border-2 px-8 py-8">
         <section class="w-full">
           <header class="flex justify-between">
-            <h2 class="text-xl">Today's Plan</h2>
-            <p>Jul 14th 2023</p>
-            <button class="h-10 w-10 rounded-full bg-black text-white">+</button
-            >
+            <div>
+              <h2 class="text-xl">Today's Plan</h2>
+              <p>Jul 14th 2023</p>
+            </div>
+            <button class="h-10 w-10 rounded-full bg-black text-white">+</button>
           </header>
         </section>
 
         <section class="w-full space-y-4">
-          <header>
-            <h2 class="text-2xl">
-              Sustainable development goals and health innovation
-            </h2>
-          </header>
+          <header><h2 class="text-2xl">Sustainable development goals and health innovation</h2></header>
           <div class="flex justify-between">
             <div class="flex">
-              <div class="h-10 w-10 rounded-full bg-black" />
-              <div class="h-10 w-10 rounded-full bg-black" />
-              <div class="h-10 w-10 rounded-full bg-black" />
+              <div class="h-10 w-10 rounded-full bg-black"></div>
+              <div class="h-10 w-10 rounded-full bg-black"></div>
+              <div class="h-10 w-10 rounded-full bg-black"></div>
             </div>
             <div class="border-l-2 pl-4">
               <h2>Medi Team meeting</h2>
@@ -49,9 +46,7 @@
           </div>
         </section>
 
-        <section
-          class="flex h-2/5 w-full flex-col justify-between space-y-4 border-2 px-8 py-8"
-        >
+        <section class="flex h-2/5 w-full flex-col justify-between space-y-4 border-2 px-8 py-8">
           <div>
             <h2 class="text-2xl">Weekly report</h2>
             <p>Track your performance</p>
@@ -74,7 +69,101 @@
         </section>
       </div>
 
-      <div class="w-1/2 border-2">asdas</div>
+      <div class="flex w-1/2 flex-wrap items-center space-y-4 border-2 px-8">
+        <section class="w-full">
+          <header class="flex justify-between">
+            <h2 class="text-xl">Monthly pinned</h2>
+            <p>View all</p>
+          </header>
+        </section>
+
+        <section class="w-full space-y-4">
+          <div class="space-y-4 border-2 px-8 py-4">
+            <header class="flex justify-between">
+              <h2>Next-gen inmmuno</h2>
+              <p>5d</p>
+            </header>
+            <div>
+              <h3>Multispectific liquids analysis</h3>
+            </div>
+            <footer>
+              <p>New feedback</p>
+            </footer>
+          </div>
+
+          <div class="spa space-y-4 border-2 px-8 py-4">
+            <header class="flex justify-between">
+              <h2>Next-gen inmmuno</h2>
+              <p>5d</p>
+            </header>
+            <div>
+              <h3>Multispectific liquids analysis</h3>
+            </div>
+            <footer>
+              <p>New feedback</p>
+            </footer>
+          </div>
+
+          <div class="space-y-4 border-2 px-8 py-4">
+            <header class="flex justify-between">
+              <h2>Next-gen inmmuno</h2>
+              <p>5d</p>
+            </header>
+            <div>
+              <h3>Multispectific liquids analysis</h3>
+            </div>
+            <footer>
+              <p>New feedback</p>
+            </footer>
+          </div>
+        </section>
+
+        <section class="w-full space-y-4">
+          <header>
+            <div class="flex justify-between">
+              <h2 class="text-xl">My learnings</h2>
+              <p>...</p>
+            </div>
+            <p>Your progress of medical lectures</p>
+          </header>
+
+          <div class="flex flex-wrap items-center justify-between py-2">
+            <div class="flex my-2 w-2/5 items-center space-x-4">
+              <div class="h-20 w-20 bg-black"></div>
+              <div>
+                <h2>Viruses</h2>
+                <p>96% progress</p>
+              </div>
+            </div>
+
+            <div class="flex my-2 w-2/5 items-center space-x-4">
+              <div class="h-20 w-20 bg-black"></div>
+              <div>
+                <h2>Viruses</h2>
+                <p>96% progress</p>
+              </div>
+            </div>
+
+            <div class="flex my-2 w-2/5 items-center space-x-4">
+              <div class="h-20 w-20 bg-black"></div>
+              <div>
+                <h2>Viruses</h2>
+                <p>96% progress</p>
+              </div>
+            </div>
+
+            <div class="flex my-2 w-2/5 items-center space-x-4">
+              <div class="h-20 w-20 bg-black"></div>
+              <div>
+                <h2>Viruses</h2>
+                <p>96% progress</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   </div>
 </div>
+
+
