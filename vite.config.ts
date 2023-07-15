0b3fe08cb9f40925d02e11074d183bf955d80947
vitalspace <myglobalsounds@gmail.com>
2023-07-15 00:29:09 -0600

    added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/admin-dashboard-analytics-ux/",
   plugins: [svelte()],
 })
