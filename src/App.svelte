e3bac641be0b02207a4ed2ef82ba8c819547690d
vitalspace <myglobalsounds@gmail.com>
2023-07-15 00:30:24 -0600

    updated project

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,80 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  console.log("Hello admin-dashboard-analytics-ux");
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<div>
+  <div class="absolute inset-0 w-72 border-2">
+    <div class="space-y-4 text-center">
+      <header class="mt-20">
+        <h2 class="text-2xl">Solarius</h2>
+      </header>
+
+      <section class="flex flex-col space-y-2">
+        <button>Dashbord</button>
+        <button>Education</button>
+        <button>Resources</button>
+        <button>Settings</button>
+      </section>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
+  <div class="absolute inset-0 ml-72 border-2 border-red-500">
+    <div class="flex fixed inset-0 ml-72">
+      <div class="flex w-1/2 flex-wrap items-center border-2 px-8 py-8">
+        <section class="w-full">
+          <header class="flex justify-between">
+            <h2 class="text-xl">Today's Plan</h2>
+            <p>Jul 14th 2023</p>
+            <button class="h-10 w-10 rounded-full bg-black text-white">+</button
+            >
+          </header>
+        </section>
+
+        <section class="w-full space-y-4">
+          <header>
+            <h2 class="text-2xl">
+              Sustainable development goals and health innovation
+            </h2>
+          </header>
+          <div class="flex justify-between">
+            <div class="flex">
+              <div class="h-10 w-10 rounded-full bg-black" />
+              <div class="h-10 w-10 rounded-full bg-black" />
+              <div class="h-10 w-10 rounded-full bg-black" />
+            </div>
+            <div class="border-l-2 pl-4">
+              <h2>Medi Team meeting</h2>
+              <p>13:30 - 14:50</p>
+            </div>
+          </div>
+        </section>
+
+        <section
+          class="flex h-2/5 w-full flex-col justify-between space-y-4 border-2 px-8 py-8"
+        >
+          <div>
+            <h2 class="text-2xl">Weekly report</h2>
+            <p>Track your performance</p>
+          </div>
+          <div>
+            <button class="border-2 px-2 py-2">VIEW REPORT</button>
+          </div>
+        </section>
+
+        <section class="flex w-full justify-between space-x-4">
+          <div class="h-72 w-1/2 border-2 pl-4 pt-8">
+            <h2 class="text-2xl">Study 78x</h2>
+            <p>In progress</p>
+          </div>
+
+          <div class="h-72 w-1/2 border-2 pl-4 pt-8">
+            <h2 class="text-2xl">Study 78x</h2>
+            <p>In progress</p>
+          </div>
+        </section>
+      </div>
+
+      <div class="w-1/2 border-2">asdas</div>
+    </div>
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+</div>
